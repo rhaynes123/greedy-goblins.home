106d587f63dba2b8fd93a29511fc9e1187730203
Richard <rahayn@yahoo.com>
2024-08-17 21:58:12 -0400

    adding more validation to the form

--- main.js
+++ main.js
@@ -1,30 +1,61 @@
-let notificationButtonId = document.getElementById('notificationButtonId');
-
-if(notificationButtonId){
-    notificationButtonId.addEventListener('click',function(event){
-        event.preventDefault();
-        let fromName = document.getElementById('name').value;
-        let fromEmail = document.getElementById('fromEmail').value;
-        let message = document.getElementById('messageId').value;
-        let subject = document.getElementById('subjectId').value;
-        const notification = {
-            name: fromName,
-            from: fromEmail,
-            reply_to: fromEmail,
-            message: message,
-            subject: subject
-        }
-        postNotification(notification)
-    });
+function htmlEncode(input) {
+    return input
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
 }
+
+function ValidNotification(notification){
+    if(notification.name === ""){
+        document.getElementById('name').focus();
+        return false;
+    }
+    if(notification.message === ""){
+        document.getElementById('messageId').focus();
+        return false;
+    }
+    const emailRegex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    if(notification.from === "" || !emailRegex.test(notification.from) ){
+        document.getElementById('fromEmail').focus();
+        return false;
+    }
+    return true;
+}
+
 async function postNotification(notification) {
+    
     let serviceId = 'service_nvfsdog';
     let templateId = 'template_deslb6d';
+    /
     let responseData = await emailjs.send(serviceId,templateId, notification);
     if(responseData.status !== 200) {
         console.log(responseData.status);
         alert('Something Went wrong');
         return
     }
+    *
     alert('Email Sent Thank You');
 }
+let notificationButtonId = document.getElementById('notificationButtonId');
+
+if(notificationButtonId){
+    notificationButtonId.addEventListener('click',function(event){
+        
+        let fromName = document.getElementById('name').value;
+        let fromEmail = document.getElementById('fromEmail').value;
+        let message = document.getElementById('messageId').value;
+        let subject = document.getElementById('subjectId').value;
+        const notification = {
+            name: htmlEncode(fromName),
+            from: htmlEncode(fromEmail),
+            reply_to: htmlEncode(fromEmail),
+            message: htmlEncode(message),
+            subject: htmlEncode(subject)
+        }
+        if(!ValidNotification(notification)){
+            return;
+        }
+        postNotification(notification)
+    });
+}
+
